21e415835289e47e6ccfb6f7bac94466c31341f6
Brindlez <106853013+Brindlez@users.noreply.github.com>
2022-07-10 12:09:41 +1200

    Update sprint.lua

--- sprint.lua
+++ sprint.lua
@@ -1,39 +1,40 @@
 local walkSpeed = 12
 local runSpeed = 20
+local froggery = false
 local tweenService = game:GetService('TweenService')
 local lp = game.Players.LocalPlayer
 local inputService = game:GetService("UserInputService")
 local function fetchHumanoid(player)
-	
+
 	if player.Character then
 		if player.Character:FindFirstChild("Humanoid") then
 			if (player.Character:FindFirstChild("Humanoid").ClassName=="Humanoid") then
 				return player.Character:FindFirstChild("Humanoid")
 			else return "Fail"
 			end
-		else return "Fail"	
+		else return "Fail"
 		end
-	else return "Fail"	
+	else return "Fail"
 	end
 end
 local function setSpeed(player,WS)
 	local humanoid = fetchHumanoid(player)
 	if humanoid=="Fail" then
 		error("Couldn't Find Humanoid")
-	else	
-		
+	else
+		froggery=true
 		tweenService:Create(humanoid,TweenInfo.new(.5 ,Enum.EasingStyle.Linear,Enum.EasingDirection.InOut),{WalkSpeed=WS}):Play()
-		
+		froggery=false
 	end
 end
-	
+
 local function handleInput(input,inputType,inputDevice)
 	if input.KeyCode==Enum.KeyCode.LeftShift or input.KeyCode==Enum.KeyCode.RightShift then
-	if inputType=="Start" then
+		if inputType=="Start" then
 			setSpeed(lp,runSpeed)
-			
+
 		elseif inputType == "End" then
-			
+
 			setSpeed(lp,walkSpeed)
 		end
 	end
@@ -53,17 +54,15 @@ local runService = game:GetService("RunService")
 local humanoid = fetchHumanoid(lp)
 
 function updateBobbleEffect()
-	local currentTime = tick()
-	if humanoid.MoveDirection.Magnitude > 0 then -- we are walking
-		local bobbleX = math.cos(currentTime * 10) * .15
-		local bobbleY = math.abs(math.sin(currentTime * 10)) * .15
-
+	local Time = tick()
+	if humanoid.MoveDirection.Magnitude <= 0 then
+		humanoid.CameraOffset = humanoid.CameraOffset * .75
+	else
+		local bobbleX = math.cos(Time * 10) * .15
+		local bobbleY = math.abs(math.sin(Time * 10)) * .15
 		local bobble = Vector3.new(bobbleX, bobbleY, 0)
 
 		humanoid.CameraOffset = humanoid.CameraOffset:lerp(bobble, .5)
-	else -- we are not walking
-		humanoid.CameraOffset = humanoid.CameraOffset * .75
-	
 	end
 end
 
